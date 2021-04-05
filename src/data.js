1d623f1c0b60065183a7c1165160360df52cac20
joshkwannacode <65772281+joshkwannacode@users.noreply.github.com>
2021-04-05 06:37:33 -0700

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,34 +1,59 @@
 import HandCafeImg from './assets/HandCafeImg.jpeg';
 import Grassfire from './assets/Grassfire.jpg';
 import Livlen from './assets/Livlen.jpeg';
+import LittleGym from './assets/LittleGym.jpg';
+import BusinessFeatured from './assets/BusinessFeatured.png';
 
 const data = [
     {
         id:"1",
+        name: "The Little Gym of Midland",
+        link: "www.thelittlegym.com/midlandtx",
+        discountDesc:"Two free Bugs Classes for children ages 4 months to 9 months",
+        desc:"Midland AF",
+        address:"4400 W. Loop 250 Frontage Rd N., Suite 103 Midland, Texas 79707",
+        phoneNum:"(432) 689-0800",
+        image: LittleGym,
+    },
+    {
+        id:"2",
         name: "Hand Cafe",
         link: "https://www.HandCafeMidland.com",
         discountDesc:"Buy One, Get One Free Coffee or Tea",
         desc:"Midland AF",
         address:"5000 Briarwood Ave Suite 105, Midland, TX 79707",
+        phoneNum:"",
         image: HandCafeImg,
     },
     {
-        id:"2",
+        id:"3",
         name: "Grassfire Lawn & Landscaping",
         link: "https://www.grassfirelawnandlandscaping.com",
         discountDesc:"First Lawn Service Free!",
         desc:"Midland AF",
         address:"Servicing Midland & Odessa",
+        phoneNum:"",
         image: Grassfire,
     },
     {
-        id:"3",
+        id:"4",
         name: "Livlen Media",
         link: "https://www.livlenmedia.com",
         discountDesc:"10% First Month of Social Media Management",
         desc:"Midland AF",
         address:"Servicing Midland & Odessa",
+        phoneNum:"",
         image: Livlen,
+    },
+    {
+        id:"5",
+        name: "Email",
+        link: "advertise@midlandaf.com",
+        discountDesc:"advertise@midlandaf.com",
+        desc:"",
+        address:"",
+        phoneNum:"",
+        image: BusinessFeatured,
     }
 ]
 
